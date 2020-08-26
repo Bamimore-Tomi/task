1187d1f0303c6bf351637f533440212650e3e350
Bamimore-Tomi <preciousgiftexpression@gmail.com>
2020-08-27 00:35:41 +0100

    making statics work

--- task/settings.py
+++ task/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Email BackEnd
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
