d12c03013f6958b2715e7c77af905a08eeeef34c
Bamimore-Tomi <preciousgiftexpression@gmail.com>
2020-08-26 23:26:04 +0100

    still on static files

--- task/settings.py
+++ task/settings.py
@@ -119,7 +119,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
