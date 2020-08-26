3a9a06f2fc4ceaa74556ce688dd1353e0047e037
Bamimore-Tomi <preciousgiftexpression@gmail.com>
2020-08-27 00:17:57 +0100

    ...

--- task/settings.py
+++ task/settings.py
@@ -119,11 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
+STATICFILES_DIRS = ([
+    os.path.join(os.path.dirname(BASE_DIR), 'Static'),    
+])
+
+STATIC_URL = '/Static/'
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'Static')
 
 # Email BackEnd
 
